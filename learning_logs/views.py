27ead97822c546c2ff59ca3d1e0449aa766689ff
Цыдыпов Владимир <vov_sed@mail.rugit config --global user.name Vladimir>
2022-06-02 18:25:47 +0300

    added form for new entries

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 
@@ -29,7 +29,7 @@ def topic(request, topic_id):
 
 
 def new_topic(request):
-    """Add new topic"""
+    """Add a new topic"""
     if request.method != 'POST':
         # No data submitted; create a blank form.
         form = TopicForm()
@@ -41,3 +41,23 @@ def new_topic(request):
 
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """Add a new entry"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = EntryForm()
+    else:
+        # POST data submitted, process data
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
+
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
