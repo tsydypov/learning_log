fa1d7e6eceb4568c3a038a4d2cfd1b03cedf93ba
Цыдыпов Владимир <vov_sed@mail.rugit config --global user.name Vladimir>
2022-06-03 01:44:18 +0300

    user can Ð register,ogin and logout

---
+++ users/urls.py
@@ -0,0 +1,14 @@
+"""Defines URL patterns for users."""
+
+from django.urls import path
+from django.contrib.auth.views import LoginView
+
+from . import views
+
+urlpatterns = [
+    # Login page, django has own login view
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout', views.logout_view, name='logout'),
+    # Registration page
+    path('register', views.register, name='register'),
+]
